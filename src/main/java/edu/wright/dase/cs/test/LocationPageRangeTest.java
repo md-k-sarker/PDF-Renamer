e148980e3e76cb76d85aaae517cc2f9bcaf4d373
Md Kamruzzaman Sarker <sarker@Mds-MacBook-Pro.local>
2017-04-10 18:28:47 -0400

    added test classes

---
+++ src/main/java/edu/wright/dase/cs/test/LocationPageRangeTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package edu.wright.dase.cs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author sarker
+ *
+ */
+public class LocationPageRangeTest {
+
+	/**
+	 * Test method for {@link edu.wright.cs.LocationPageRange#LocationPageRange()}.
+	 */
+	@Test
+	public void testLocationPageRange() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wright.cs.LocationPageRange#initialize(java.nio.file.Path)}.
+	 */
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wright.cs.LocationPageRange#extractPageRange()}.
+	 */
+	@Test
+	public void testExtractPageRange() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wright.cs.LocationPageRange#extractLocation()}.
+	 */
+	@Test
+	public void testExtractLocation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.wright.cs.LocationPageRange#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
